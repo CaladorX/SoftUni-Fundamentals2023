d6641ac377d3c3a14d066741763ff6e65cff2da3
Stefan Popov <avalu1098@gmail.com>
2023-09-28 21:12:30 +0100

    arrays

--- Homeworks-Lab/05.Arrays - Lab/Arrays-Lab/03.RoundingNumbers/Program.cs
+++ Homeworks-Lab/05.Arrays - Lab/Arrays-Lab/03.RoundingNumbers/Program.cs
@@ -1,20 +1,25 @@
-﻿namespace _03.RoundingNumbers
+﻿using System;
+
+namespace _03.RoundingNumbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             double[] number = Console.ReadLine()
-                .Split()
+                .Split(" ")
                 .Select(double.Parse)
                 .ToArray();
 
+            int[] roundNumber = new int[number.Length];
+
             for (int i = 0; i < number.Length; i++)
             {
-                double numbers = number[i];
-                double roundNumber = Math.Round(numbers, MidpointRounding.AwayFromZero);
-
-                Console.WriteLine($"{Convert.ToDecimal(numbers)} => {Convert.ToDecimal(roundNumber)}");
+                roundNumber[i] = (int)Math.Round(number[i], MidpointRounding.AwayFromZero);
+            }
+            for (int i = 0; i < number.Length; i++)
+            {
+                Console.WriteLine($"{number[i]} => {roundNumber[i]}");
             }
         }
     }
