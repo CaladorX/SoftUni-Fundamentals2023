903a61eb409ef4ad6815bfb636fbfb1e25b6496c
Stefan Popov <avalu1098@gmail.com>
2023-10-02 01:00:20 +0100

    Arrays-Exercise

---
+++ Homeworks-Lab/06.Arrays - Exercise/Exercise/01.Train/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] wagons = new int[n];
+            int sumPeople = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                wagons[i] = number;
+                sumPeople += number;
+            }
+
+            Console.WriteLine(string.Join(" ", wagons));
+            Console.WriteLine(sumPeople);
+        }
+    }
+}
---
+++ Homeworks-Lab/06.Arrays - Exercise/Exercise/03.Zig-ZagArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _03.Zig_ZagArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // INPUT
+            int n = int.Parse(Console.ReadLine());
+            string[] firstArr = new string[n];
+            string[] secondArr = new string[n];
+            bool isFirstSelected = true;
+
+            // OPERATION
+            for (int i = 0; i < n; i++)
+            {
+                string numbers = Console.ReadLine();
+                string[] numArr = numbers.Split();
+
+                if (isFirstSelected == true)
+                {
+                    firstArr[i] = numArr[0];
+                    secondArr[i] = numArr[1];
+                }
+                else
+                {
+                    firstArr[i] = numArr[1];
+                    secondArr[i] = numArr[0];
+                }
+                
+                isFirstSelected = !isFirstSelected;
+
+            }
+
+            //OUTPUT
+            Console.WriteLine(string.Join(" ", firstArr));
+            Console.WriteLine(string.Join(" ", secondArr));
+
+        }
+    }
+}
