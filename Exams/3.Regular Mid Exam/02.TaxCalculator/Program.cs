6f8f02dc7b01ebab8c7b6f8e8caf4023da5861e0
Stefan Popov <avalu1098@gmail.com>
2023-10-22 12:19:08 +0100

    Exam

--- Exams/3.Regular Mid Exam/02.TaxCalculator/Program.cs
+++ Exams/3.Regular Mid Exam/02.TaxCalculator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _02.TaxCalculator
+﻿using System;
+
+namespace _02.TaxCalculator
 {
     internal class Program
     {
@@ -16,48 +18,46 @@
                 int years = int.Parse(vehicleDetails[1]);
                 int kilometers = int.Parse(vehicleDetails[2]);
 
-                double tax = CalculateTax(carType, years, kilometers);
-                totalTax += tax;
+                double tax = 0;
 
+                if (carType == "family")
+                {
+                    tax = 50 - 5 * years;
+                    if (kilometers > 3000)
+                    {
+                        int exceedingKilometers = kilometers - 3000;
+                        tax += exceedingKilometers / 3000 * 12;
+                    }
+                }
+                else if (carType == "heavyDuty")
+                {
+                    tax = 80 - 8 * years;
+                    if (kilometers > 9000)
+                    {
+                        int exceedingKilometers = kilometers - 9000;
+                        tax += exceedingKilometers / 9000 * 14;
+                    }
+                }
+                else if (carType == "sports")
+                {
+                    tax = 100 - 9 * years;
+                    if (kilometers > 2000)
+                    {
+                        int exceedingKilometers = kilometers - 2000;
+                        tax += exceedingKilometers / 2000 * 18;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid car type.");
+                    continue; // Skip to the next iteration
+                }
+
+                totalTax += tax;
                 Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
             }
 
             Console.WriteLine($"The National Revenue Agency will collect {totalTax:f2} euros in taxes.");
-        }
-
-        static double CalculateTax(string carType, int years, int kilometers)
-        {
-            double tax = 0;
-
-            if (carType == "family")
-            {
-                tax = 50 - 5 * years;
-                if (kilometers > 3000)
-                {
-                    int exceedingKilometers = kilometers - 3000;
-                    tax += exceedingKilometers / 3000 * 12;
-                }
-            }
-            else if (carType == "heavyDuty")
-            {
-                tax = 80 - 8 * years;
-                if (kilometers > 9000)
-                {
-                    int exceedingKilometers = kilometers - 9000;
-                    tax += exceedingKilometers / 9000 * 14;
-                }
-            }
-            else if (carType == "sports")
-            {
-                tax = 100 - 9 * years;
-                if (kilometers > 2000)
-                {
-                    int exceedingKilometers = kilometers - 2000;
-                    tax += exceedingKilometers / 2000 * 18;
-                }
-            }
-
-            return tax;
         }
     }
 }
