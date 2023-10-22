3a2a4fce591f62418a81f2adb3c6410606febc15
Stefan Popov <avalu1098@gmail.com>
2023-10-22 11:55:47 +0100

    Exam

---
+++ Exams/3.Regular Mid Exam/01.ExperienceGaining/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace _01.ExperienceGaining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double experience = double.Parse(Console.ReadLine());
+            int battles = int.Parse(Console.ReadLine());
+            double expBar = 0;
+            int battleNum = 0;
+            bool isEnough = false;
+
+            for (int i = 1; i <= battles; i++)
+            {
+                double expPerBattle = double.Parse(Console.ReadLine());
+
+                battleNum++;
+
+                if (i % 3 == 0)
+                {
+                    expPerBattle *= 1.15;
+                }
+
+                if (i % 5 == 0)
+                {
+                    expPerBattle *= 0.90;
+                }
+
+                if (i % 15 == 0)
+                {
+                    expPerBattle *= 1.05;
+                }
+
+                expBar += expPerBattle;
+
+                if (expBar >= experience)
+                {
+                    isEnough = true;
+                    break;
+                }
+
+            }
+
+            if (isEnough)
+            {
+                Console.WriteLine($"Player successfully collected his needed experience for {battleNum} battles.");
+            }
+            else
+            {
+                double neededExp = experience - expBar;
+                Console.WriteLine($"Player was not able to collect the needed experience, {neededExp:f2} more needed.");
+            }          
+        }
+    }
+}
---
+++ Exams/3.Regular Mid Exam/02.TaxCalculator/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _02.TaxCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] vehicles = input.Split(">>");
+
+            double totalTax = 0;
+
+            foreach (string vehicle in vehicles)
+            {
+                string[] vehicleDetails = vehicle.Split(' ');
+                string carType = vehicleDetails[0];
+                int years = int.Parse(vehicleDetails[1]);
+                int kilometers = int.Parse(vehicleDetails[2]);
+
+                double tax = CalculateTax(carType, years, kilometers);
+                totalTax += tax;
+
+                Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
+            }
+
+            Console.WriteLine($"The National Revenue Agency will collect {totalTax:f2} euros in taxes.");
+        }
+
+        static double CalculateTax(string carType, int years, int kilometers)
+        {
+            double tax = 0;
+
+            if (carType == "family")
+            {
+                tax = 50 - 5 * years;
+                if (kilometers > 3000)
+                {
+                    int exceedingKilometers = kilometers - 3000;
+                    tax += exceedingKilometers / 3000 * 12;
+                }
+            }
+            else if (carType == "heavyDuty")
+            {
+                tax = 80 - 8 * years;
+                if (kilometers > 9000)
+                {
+                    int exceedingKilometers = kilometers - 9000;
+                    tax += exceedingKilometers / 9000 * 14;
+                }
+            }
+            else if (carType == "sports")
+            {
+                tax = 100 - 9 * years;
+                if (kilometers > 2000)
+                {
+                    int exceedingKilometers = kilometers - 2000;
+                    tax += exceedingKilometers / 2000 * 18;
+                }
+            }
+
+            return tax;
+        }
+    }
+}
