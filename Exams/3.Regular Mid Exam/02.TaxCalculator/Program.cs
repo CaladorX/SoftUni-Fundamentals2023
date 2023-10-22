db6280bfe3b200cd1842982a6e052f83b9aeb7c0
Stefan Popov <avalu1098@gmail.com>
2023-10-22 12:26:03 +0100

    Exam

--- Exams/3.Regular Mid Exam/02.TaxCalculator/Program.cs
+++ Exams/3.Regular Mid Exam/02.TaxCalculator/Program.cs
@@ -1,63 +1,80 @@
-﻿using System;
-
-namespace _02.TaxCalculator
+﻿namespace _02.TaxCalculator
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string input = Console.ReadLine();
-            string[] vehicles = input.Split(">>");
+            string[] vehicles = Console.ReadLine().Split(">>");
+            double TotalTax = 0;
 
-            double totalTax = 0;
-
-            foreach (string vehicle in vehicles)
+            while (vehicles.Length > 0)
             {
-                string[] vehicleDetails = vehicle.Split(' ');
-                string carType = vehicleDetails[0];
-                int years = int.Parse(vehicleDetails[1]);
-                int kilometers = int.Parse(vehicleDetails[2]);
-
+                string[] currentCar = vehicles[0].Split(' ');
+                string carType = currentCar[0];
+                int years = int.Parse(currentCar[1]);
+                int kilometers = int.Parse(currentCar[2]);
                 double tax = 0;
 
                 if (carType == "family")
                 {
-                    tax = 50 - 5 * years;
-                    if (kilometers > 3000)
+                    tax = 50;
+                    for (int year = 1; year <= years; year++)
                     {
-                        int exceedingKilometers = kilometers - 3000;
-                        tax += exceedingKilometers / 3000 * 12;
+                        tax -= 5;
                     }
+
+                    while (kilometers - 3000 >= 0)
+                    {
+                        tax += 12;
+                        kilometers -= 3000;
+                    }
+
+                    TotalTax += tax;
+                    Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
                 }
                 else if (carType == "heavyDuty")
                 {
-                    tax = 80 - 8 * years;
-                    if (kilometers > 9000)
+                    tax = 80;
+                    for (int year = 1; year <= years; year++)
                     {
-                        int exceedingKilometers = kilometers - 9000;
-                        tax += exceedingKilometers / 9000 * 14;
+                        tax -= 8;
                     }
+
+                    while (kilometers - 9000 >= 0)
+                    {
+                        tax += 14;
+                        kilometers -= 9000;
+                    }
+
+                    TotalTax += tax;
+                    Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
                 }
                 else if (carType == "sports")
                 {
-                    tax = 100 - 9 * years;
-                    if (kilometers > 2000)
+                    tax = 100;
+                    for (int year = 1; year <= years; year++)
                     {
-                        int exceedingKilometers = kilometers - 2000;
-                        tax += exceedingKilometers / 2000 * 18;
+                        tax -= 9;
                     }
+
+                    while (kilometers - 2000 >= 0)
+                    {
+                        tax += 18;
+                        kilometers -= 2000;
+                    }
+
+                    TotalTax += tax;
+                    Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
                 }
                 else
                 {
                     Console.WriteLine("Invalid car type.");
-                    continue; // Skip to the next iteration
                 }
 
-                totalTax += tax;
-                Console.WriteLine($"A {carType} car will pay {tax:f2} euros in taxes.");
+                vehicles = vehicles[1..];
             }
 
-            Console.WriteLine($"The National Revenue Agency will collect {totalTax:f2} euros in taxes.");
+            Console.WriteLine($"The National Revenue Agency will collect {TotalTax:F2} euros in taxes.");
         }
     }
 }
